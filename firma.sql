f82e2c0d1d1868446e3ff1926b1ce53bd495408d
BIKTOPBB <BIKTOPBB@gmail.com>
2018-08-08 18:43:51 +0300

    пример БД транспортно-эксп компании

--- firma.sql
+++ firma.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1:3306
--- Время создания: Авг 06 2018 г., 23:01
+-- Время создания: Авг 08 2018 г., 18:41
 -- Версия сервера: 5.6.38
 -- Версия PHP: 5.5.38
 
@@ -29,20 +29,18 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `client` (
-  `id` int(11) UNSIGNED NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `sity_name` varchar(255) NOT NULL
+  `id` int(12) UNSIGNED NOT NULL,
+  `clientName` varchar(255) NOT NULL,
+  `sity_id` int(12) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Дамп данных таблицы `client`
 --
 
-INSERT INTO `client` (`id`, `name`, `sity_name`) VALUES
-(1, 'test1', 'Dnepr'),
-(3, 'Чп Иванов В.В.', 'Dnepr'),
-(4, 'ООО Секрет', 'Kiev'),
-(2, 'test2', 'Liev');
+INSERT INTO `client` (`id`, `clientName`, `sity_id`) VALUES
+(5, 'ФЛП Пупкин В.В.', 1),
+(6, 'ООО Клубничка', 4);
 
 -- --------------------------------------------------------
 
@@ -52,22 +50,21 @@ INSERT INTO `client` (`id`, `name`, `sity_name`) VALUES
 
 CREATE TABLE `deal` (
   `id` int(12) UNSIGNED NOT NULL,
-  `client_name` varchar(255) NOT NULL,
-  `sity_name_start` varchar(255) NOT NULL,
-  `sity_name_finish` varchar(255) NOT NULL,
-  `drivers_name` varchar(255) NOT NULL,
-  `price` int(20) NOT NULL,
-  `method_name` varchar(255) NOT NULL
+  `client_id` int(12) UNSIGNED DEFAULT NULL,
+  `driver_id` int(12) UNSIGNED NOT NULL,
+  `sityStart_id` int(12) UNSIGNED NOT NULL,
+  `sityFinish_id` int(12) UNSIGNED NOT NULL,
+  `price` varchar(45) NOT NULL,
+  `method_id` int(12) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Дамп данных таблицы `deal`
 --
 
-INSERT INTO `deal` (`id`, `client_name`, `sity_name_start`, `sity_name_finish`, `drivers_name`, `price`, `method_name`) VALUES
-(1, 'test1', 'Dnepr', 'Kiev', 'ФЛП Пупкин', 5000, 'Б/Н с НДС'),
-(2, 'ООО Секрет', 'Dnepr', 'Kiev', 'ФЛП Пупкин', 6000, 'Б/Н с НДС'),
-(3, 'Чп Иванов В.В.', 'Lviv', 'Dnepr', 'ФЛП Пупкин', 18000, 'Б/Н с НДС');
+INSERT INTO `deal` (`id`, `client_id`, `driver_id`, `sityStart_id`, `sityFinish_id`, `price`, `method_id`) VALUES
+(1, 6, 2, 1, 1, '3200', 1),
+(2, 5, 1, 4, 1, '8000', 1);
 
 -- --------------------------------------------------------
 
@@ -77,17 +74,17 @@ INSERT INTO `deal` (`id`, `client_name`, `sity_name_start`, `sity_name_finish`, 
 
 CREATE TABLE `drivers` (
   `id` int(12) UNSIGNED NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `sity_name` varchar(255) NOT NULL
+  `driverName` varchar(255) NOT NULL,
+  `sity_id` int(12) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Дамп данных таблицы `drivers`
 --
 
-INSERT INTO `drivers` (`id`, `name`, `sity_name`) VALUES
-(2, 'ФЛП Дудкин', 'Dnepr'),
-(1, 'ФЛП Пупкин', 'Lviv');
+INSERT INTO `drivers` (`id`, `driverName`, `sity_id`) VALUES
+(1, 'ФЛП Кушнир С.В.', 1),
+(2, 'ООО АРТА', 1);
 
 -- --------------------------------------------------------
 
@@ -97,18 +94,18 @@ INSERT INTO `drivers` (`id`, `name`, `sity_name`) VALUES
 
 CREATE TABLE `method` (
   `id` int(12) UNSIGNED NOT NULL,
-  `name` varchar(255) DEFAULT NULL
+  `methodName` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Дамп данных таблицы `method`
 --
 
-INSERT INTO `method` (`id`, `name`) VALUES
-(2, 'Б/Н единый'),
+INSERT INTO `method` (`id`, `methodName`) VALUES
 (1, 'Б/Н с НДС'),
-(4, 'Нал'),
-(3, 'Софт');
+(2, 'Б/Н единый'),
+(3, 'Софт'),
+(4, 'Нал');
 
 -- --------------------------------------------------------
 
@@ -117,19 +114,18 @@ INSERT INTO `method` (`id`, `name`) VALUES
 --
 
 CREATE TABLE `sity` (
-  `id` int(11) NOT NULL,
-  `name` varchar(255) NOT NULL
+  `id` int(12) UNSIGNED NOT NULL,
+  `sityName` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Дамп данных таблицы `sity`
 --
 
-INSERT INTO `sity` (`id`, `name`) VALUES
+INSERT INTO `sity` (`id`, `sityName`) VALUES
 (1, 'Dnepr'),
-(4, 'Kiev'),
-(3, 'Liev'),
-(2, 'Lviv');
+(2, 'Lviv'),
+(4, 'Kiev');
 
 --
 -- Индексы сохранённых таблиц
@@ -139,45 +135,38 @@ INSERT INTO `sity` (`id`, `name`) VALUES
 -- Индексы таблицы `client`
 --
 ALTER TABLE `client`
-  ADD PRIMARY KEY (`id`,`sity_name`,`name`),
-  ADD KEY `sity_name` (`sity_name`),
-  ADD KEY `name` (`name`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `sity_id` (`sity_id`);
 
 --
 -- Индексы таблицы `deal`
 --
 ALTER TABLE `deal`
-  ADD PRIMARY KEY (`id`,`client_name`,`sity_name_start`,`sity_name_finish`,`drivers_name`),
-  ADD KEY `client_name` (`client_name`),
-  ADD KEY `sity_name_start_fk_idx` (`sity_name_start`),
-  ADD KEY `sity_name_finish_fk_idx` (`sity_name_finish`),
-  ADD KEY `drivers_name_fk_idx` (`drivers_name`),
-  ADD KEY `drivers_name` (`drivers_name`),
-  ADD KEY `idx_deal_method_name` (`method_name`),
-  ADD KEY `method_name_fk_idx` (`method_name`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `client_id` (`client_id`),
+  ADD KEY `driver_id` (`driver_id`),
+  ADD KEY `sityStart_id` (`sityStart_id`),
+  ADD KEY `sityFinish_id` (`sityFinish_id`),
+  ADD KEY `method_id` (`method_id`);
 
 --
 -- Индексы таблицы `drivers`
 --
 ALTER TABLE `drivers`
-  ADD PRIMARY KEY (`id`,`sity_name`,`name`),
-  ADD KEY `sity_name_fk_idx` (`sity_name`),
-  ADD KEY `name` (`name`);
+  ADD PRIMARY KEY (`id`) USING BTREE,
+  ADD KEY `sity_id` (`sity_id`) USING BTREE;
 
 --
 -- Индексы таблицы `method`
 --
 ALTER TABLE `method`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `id_UNIQUE` (`id`),
-  ADD KEY `idx_method_name` (`name`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Индексы таблицы `sity`
 --
 ALTER TABLE `sity`
-  ADD PRIMARY KEY (`id`,`name`),
-  ADD KEY `name` (`name`);
+  ADD PRIMARY KEY (`id`) USING BTREE;
 
 --
 -- AUTO_INCREMENT для сохранённых таблиц
@@ -187,13 +176,13 @@ ALTER TABLE `sity`
 -- AUTO_INCREMENT для таблицы `client`
 --
 ALTER TABLE `client`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(12) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT для таблицы `deal`
 --
 ALTER TABLE `deal`
-  MODIFY `id` int(12) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(12) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT для таблицы `drivers`
@@ -211,7 +200,7 @@ ALTER TABLE `method`
 -- AUTO_INCREMENT для таблицы `sity`
 --
 ALTER TABLE `sity`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(12) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- Ограничения внешнего ключа сохраненных таблиц
@@ -221,23 +210,23 @@ ALTER TABLE `sity`
 -- Ограничения внешнего ключа таблицы `client`
 --
 ALTER TABLE `client`
-  ADD CONSTRAINT `fk` FOREIGN KEY (`sity_name`) REFERENCES `sity` (`name`);
+  ADD CONSTRAINT `client_sity_id_fk` FOREIGN KEY (`sity_id`) REFERENCES `sity` (`id`);
 
 --
 -- Ограничения внешнего ключа таблицы `deal`
 --
 ALTER TABLE `deal`
-  ADD CONSTRAINT `client_name_fk` FOREIGN KEY (`client_name`) REFERENCES `client` (`name`),
-  ADD CONSTRAINT `drivers_name_fk` FOREIGN KEY (`drivers_name`) REFERENCES `drivers` (`name`),
-  ADD CONSTRAINT `method_name_fk` FOREIGN KEY (`method_name`) REFERENCES `method` (`name`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `sity_name_finish_fk` FOREIGN KEY (`sity_name_finish`) REFERENCES `sity` (`name`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `sity_name_start_fk` FOREIGN KEY (`sity_name_start`) REFERENCES `sity` (`name`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+  ADD CONSTRAINT `client_id_fk` FOREIGN KEY (`client_id`) REFERENCES `client` (`id`),
+  ADD CONSTRAINT `driver_id_fk` FOREIGN KEY (`driver_id`) REFERENCES `drivers` (`id`),
+  ADD CONSTRAINT `method_id` FOREIGN KEY (`method_id`) REFERENCES `method` (`id`),
+  ADD CONSTRAINT `sityFinish_id` FOREIGN KEY (`sityFinish_id`) REFERENCES `sity` (`id`),
+  ADD CONSTRAINT `sityStart_id` FOREIGN KEY (`sityStart_id`) REFERENCES `sity` (`id`);
 
 --
 -- Ограничения внешнего ключа таблицы `drivers`
 --
 ALTER TABLE `drivers`
-  ADD CONSTRAINT `sity_name_fk` FOREIGN KEY (`sity_name`) REFERENCES `sity` (`name`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+  ADD CONSTRAINT `driver_sity_id_fk` FOREIGN KEY (`sity_id`) REFERENCES `sity` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
